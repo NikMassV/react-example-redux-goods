8c53a7edf58f8976c38235eb80ac355476875403
NikMassV <slipknoshka777@gmail.com>
2024-02-15 14:20:06 +0100

    init

---
+++ src/containers/GoodsList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {
+    selectGoods
+} from '../store/goodsSlice';
+
+import Goods from '../components/Goods';
+/**
+ * get data from store
+ * list data
+ */
+function GoodsList() {
+    const goods = useSelector(selectGoods);
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            <div class="goods-field">
+                {goods.map(item => <Goods title={item.title} cost={item.cost} image={item.image}
+                    articul={item.articul} key={item.articul} />)}
+            </div>
+        </>
+    );
+}
+
+export default GoodsList;
